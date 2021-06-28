896adb573bfaad843704eb58b4c73ee95f98485d
qy <30001677@qq.com>
2021-06-28 18:24:49 +0800

    update

--- src/mdf/mdf.ts
+++ src/mdf/mdf.ts
@@ -8,53 +8,54 @@ import { prettierFormat } from '@mdfjs/utils';
  */
 
 function importsfy(sourceList: string[]) {
-  return sourceList.map(source => `import '${source}';`);
+  return sourceList.map((source) => `import '${source}';`);
 }
 
-export default function(api: IApi) {
+export default function (api: IApi) {
   api.onCodeGenerate({
     name: 'genReact',
     fn() {
-    const { Mustache, PluginType, paths } = api;
-    const { project } = api.getConfig();
-    const tpl = api.getFile(join(__dirname, 'mdf.tpl'));
+      const { Mustache, PluginType, paths } = api;
+      const { project } = api.getConfig();
+      const tpl = api.getFile(join(__dirname, 'mdf.tpl'));
 
-    const content = Mustache.render(tpl, {
-      useDva: project.framework === 'dva',
+      const content = Mustache.render(tpl, {
+        useDva: project.framework === 'dva',
 
-      importsPolyfill: importsfy(
-        api.invokePlugin({
-          key: 'importsPolyfill',
-          type: PluginType.add,
-          initValue: [],
-        }),
-      ).join('\r\n'),
+        importsPolyfill: importsfy(
+          api.invokePlugin({
+            key: 'importsPolyfill',
+            type: PluginType.add,
+            initValue: [],
+          }),
+        ).join('\r\n'),
 
-      importsAhead: importsfy(
-        api.invokePlugin({
-          key: 'importsAhead',
-          type: PluginType.add,
-          initValue: [],
-        }),
-      ).join('\r\n'),
+        importsAhead: importsfy(
+          api.invokePlugin({
+            key: 'importsAhead',
+            type: PluginType.add,
+            initValue: [],
+          }),
+        ).join('\r\n'),
 
-      importsBehind: importsfy(
-        api.invokePlugin({
-          key: 'importsBehind',
-          type: PluginType.add,
-          initValue: [],
-        }),
-      ).join('\r\n'),
+        importsBehind: importsfy(
+          api.invokePlugin({
+            key: 'importsBehind',
+            type: PluginType.add,
+            initValue: [],
+          }),
+        ).join('\r\n'),
 
-      globalCode: api
-        .invokePlugin({
-          key: 'globalCode',
-          type: PluginType.add,
-          initValue: [],
-        })
-        .join('\r\n'),
-    });
+        globalCode: api
+          .invokePlugin({
+            key: 'globalCode',
+            type: PluginType.add,
+            initValue: [],
+          })
+          .join('\r\n'),
+      });
 
-    api.writeFile(`${paths.absTmpPath}/mdf.tsx`, prettierFormat(content));
-  }});
+      api.writeFile(`${paths.absTmpPath}/mdf.tsx`, prettierFormat(content));
+    },
+  });
 }
