4b899ae621c840b7f2fc9dd8f23076b8c6f79ce4
aJean <30001677@qq.com>
2021-09-08 18:05:59 +0800

    update

--- src/index.ts
+++ src/index.ts
@@ -37,3 +37,67 @@ export default function (api: IApi) {
 
   return { presets };
 }
+
+/**
+ * es-lint config
+ */
+export function lint(opts: any) {
+  Object.assign(opts.settings, {
+    react: {
+      version: 'detect',
+    },
+  });
+
+  opts.plugins.push('react', 'react-hooks');
+
+  Object.assign(opts.rules, {
+    'no-restricted-properties': [
+      'error',
+      {
+        object: 'require',
+        property: 'ensure',
+        message:
+          'Please use import() instead. More info: https://facebook.github.io/create-react-app/docs/code-splitting',
+      },
+      {
+        object: 'System',
+        property: 'import',
+        message:
+          'Please use import() instead. More info: https://facebook.github.io/create-react-app/docs/code-splitting',
+      },
+    ],
+
+    'react-hooks/exhaustive-deps': 'warn',
+    // https://github.com/yannickcr/eslint-plugin-react/tree/master/docs/rules
+    'react/forbid-foreign-prop-types': ['warn', { allowInPropTypes: true }],
+    'react/jsx-no-comment-textnodes': 'warn',
+    'react/jsx-no-duplicate-props': 'warn',
+    'react/jsx-no-target-blank': 'warn',
+    'react/jsx-no-undef': 'error',
+    'react/jsx-pascal-case': [
+      'warn',
+      {
+        allowAllCaps: true,
+        ignore: [],
+      },
+    ],
+    'react/jsx-uses-react': 'warn',
+    'react/jsx-uses-vars': 'warn',
+    'react/no-danger-with-children': 'warn',
+    // Disabled because of undesirable warnings
+    // See https://github.com/facebook/create-react-app/issues/5204 for
+    // blockers until its re-enabled
+    // 'react/no-deprecated': 'warn',
+    'react/no-direct-mutation-state': 'warn',
+    'react/no-is-mounted': 'warn',
+    'react/no-typos': 'error',
+    'react/react-in-jsx-scope': 'error',
+    'react/require-render-return': 'error',
+    'react/style-prop-object': 'warn',
+    'react/jsx-curly-brace-presence': ['error', { props: 'never', children: 'never' }],
+    'react/sort-comp': ['error'],
+    'jsx-quotes': ['error', 'prefer-double'],
+  });
+
+  return opts;
+}
